11559f6c3acb24589cc73c54864a7f592d4989c1
Nabil Gishkori <nabilgishkori@gmail.com>
2019-02-01 17:34:58 +0000

    Complete assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions perform a matrix inversion on a matrix provided by the user. 
+## If the calculation has been performed before then it will use the cached 
+## result to save time.
 
-## Write a short comment describing this function
+## This function creates a special list containing functions that allow 
+## manipulation of the provided matrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverted <- NULL
+        set <- function(y) {
+                x <<- y
+                inverted <<- NULL
+        }
+        get <- function() {
+                x
+        }
+        setinverted <- function(inv) {
+                inverted <<- inv
+        }
+        getinverted <- function() {
+                inverted
+        }
+        list(
+                set = set,
+                get = get,
+                setinverted = setinverted,
+                getinverted = getinverted
+        )
+        
 }
 
 
-## Write a short comment describing this function
+## This function will search to see if there is a cached version of the matrix 
+## inversion. If so it will return it, otherwise it will first perform the 
+## inversion.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverted <- x$getinverted()
+        if (!is.null(inverted)) {
+                message("Using cached inversion")
+                return(inverted)
+        }
+        matrix <- x$get()
+        inverted <- solve(matrix)
+        x$setinverted(inverted)
+        inverted
 }
